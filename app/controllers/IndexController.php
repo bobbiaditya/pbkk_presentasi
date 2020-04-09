df3d71bcdf581da00e368176c74880e04cecc341
bobbiaditya <bobbi.aditya@gmail.com>
2020-04-09 14:11:34 +0700

    tes

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -6,7 +6,7 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
-        return '<h1>Hello!</h1>';
+        return 'Jihan Cepat Sembuh';
     }
 
     public function sayHalloAction()
