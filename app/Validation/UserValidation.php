93c1312e114664fda3f328cf64bd82cf56c64260
bobbiaditya <bobbi.aditya@gmail.com>
2020-04-10 20:38:27 +0700

    tambah event validation

--- app/Validation/UserValidation.php
+++ app/Validation/UserValidation.php
@@ -35,4 +35,16 @@ class UserValidation extends Validation
             )
         );
     }
+    // public function beforeValidation()
+    // {
+    //     // echo('before validation<br>');
+    //     echo('return false sehingga validation tidak dilakukan');
+    //     return false;
+    // }
+
+    // public function afterValidation($data, $entity, $messages)
+    // {
+    //     // ... Add additional messages or perform more validations
+    //     echo('masuk after validation<br>');
+    // }
 }
